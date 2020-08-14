ba10589127ccf187e169fda8e06ffbfc94a5d371
bilgeyucel <bilgeyucel@sabanciuniv.edu>
2020-08-14 16:36:43 +0300

    Update `react/jsx-curly-brace-presence` rule
    * Force using curly braces around strings if they are children
    
    Issue: #10
    PR: #18

--- index.js
+++ index.js
@@ -148,7 +148,7 @@ module.exports = {
       "error",
       {
         "props": "always",
-        "children": "ignore"
+        "children": "always"
       }
     ],
     "react/jsx-sort-props": [
